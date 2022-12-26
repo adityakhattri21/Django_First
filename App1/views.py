4440dd04d770099cd5be2f40da36a22c510739ea
adityakhattri21 <adityakhattri123@gmail.com>
2022-12-27 01:25:04 +0530

    Added db to form

--- App1/views.py
+++ App1/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render , HttpResponse
+from datetime import datetime
+from App1.models import Contact
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
@@ -16,7 +19,15 @@ def about(request):
 def services(request):
     return render(request , 'services.html')
     # return HttpResponse("This is services page")
-    
+
+@csrf_exempt   
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name = name , email=email , phone = phone , desc = desc , date = datetime.today())
+        contact.save();
     return render(request , 'contact.html')
     # return HttpResponse("This is contact page")
